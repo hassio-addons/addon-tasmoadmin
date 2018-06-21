55844ec4746fa3a3169f5dfa258b963c18627344
Franck Nijhof <frenck@geekchimp.com>
2018-06-22 00:11:51 +0200

    :ambulance: Fixes incorrect data directory

--- tasmoadmin/rootfs/etc/cont-init.d/13-persistent-data.sh
+++ tasmoadmin/rootfs/etc/cont-init.d/13-persistent-data.sh
@@ -14,8 +14,8 @@ if ! hass.directory_exists "/data/tasmoadmin"; then
 
     # Ensure file permissions
     chown -R nginx:nginx /data/tasmoadmin
-    find /data/tasmoadmin/tasmoadmin -not -perm 0644 -type f -exec chmod 0644 {} \;
-    find /data/tasmoadmin/tasmoadmin -not -perm 0755 -type d -exec chmod 0755 {} \;
+    find /data/tasmoadmin -not -perm 0644 -type f -exec chmod 0644 {} \;
+    find /data/tasmoadmin -not -perm 0755 -type d -exec chmod 0755 {} \;
 fi
 
 hass.log.debug 'Symlinking data directory to persistent storage location...'
