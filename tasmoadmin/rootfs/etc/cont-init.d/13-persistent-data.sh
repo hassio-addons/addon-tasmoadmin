a0ddbd8370cad94d5ccb5570a774e4f448ecc3a6
Franck Nijhof <frenck@geekchimp.com>
2018-06-21 23:01:46 +0200

    :ambulance: Fixes upstream changed webroot location

--- tasmoadmin/rootfs/etc/cont-init.d/13-persistent-data.sh
+++ tasmoadmin/rootfs/etc/cont-init.d/13-persistent-data.sh
@@ -10,14 +10,14 @@ if ! hass.directory_exists "/data/tasmoadmin"; then
     hass.log.debug 'Data directory not initialized, doing that now...'
 
     # Setup structure
-    cp -R /var/www/tasmoadmin/data /data/tasmoadmin
+    cp -R /var/www/tasmoadmin/tasmoadmin/data /data/tasmoadmin
 
     # Ensure file permissions
     chown -R nginx:nginx /data/tasmoadmin
-    find /data/tasmoadmin -not -perm 0644 -type f -exec chmod 0644 {} \;
-    find /data/tasmoadmin -not -perm 0755 -type d -exec chmod 0755 {} \;
+    find /data/tasmoadmin/tasmoadmin -not -perm 0644 -type f -exec chmod 0644 {} \;
+    find /data/tasmoadmin/tasmoadmin -not -perm 0755 -type d -exec chmod 0755 {} \;
 fi
 
 hass.log.debug 'Symlinking data directory to persistent storage location...'
-rm -f -r /var/www/tasmoadmin/data
-ln -s /data/tasmoadmin /var/www/tasmoadmin/data
+rm -f -r /var/www/tasmoadmin/tasmoadmin/data
+ln -s /data/tasmoadmin /var/www/tasmoadmin/tasmoadmin/data
